67450e8c80257ec42fa6875ab4867f77cef2dc62
minhquan0902 <minhquan0902@gmail.com>
2020-04-16 19:50:43 +0800

    commit

---
+++ Prac_04/quick_picks.py
@@ -0,0 +1,16 @@
+import random
+
+def main():
+    quick = int(input("How many 'Quick_pick' you wish to generate? "))
+    for i in range(0,quick):
+        list=[random.randint(1,45) for i in range (6)]
+        list.sort()
+        for count in range(0, len(list)):
+            print(list[count], end =" ".center(1))
+
+        print("".center(1))
+
+
+
+main()
+
